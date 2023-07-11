6814fd50abc0e494ca9db35db3cfb6880ea13e1b
shantanu <iamshantanudas17@gmail.com>
2023-07-11 11:14:52 +0530

    backend uploaded

---
+++ EurekaServer/src/main/java/com/example/EurekaServer/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
